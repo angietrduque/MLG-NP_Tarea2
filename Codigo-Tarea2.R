f6159584c40073f5731f4753f1141f6c6b17785a
Cesar A. Saavedra Vanegas <31328713+cesarsaa@users.noreply.github.com>
2020-10-10 14:30:16 -0500

    Datos.txt

--- Codigo-Tarea2.R
+++ Codigo-Tarea2.R
@@ -34,7 +34,12 @@ suppressMessages(library(CCA))
 suppressMessages(library(plotly))
 suppressMessages(library(broom))
 suppressMessages(library(readr))
+suppressMessages(library(readxl))
 
+#----------------------------------------------------------------------------------------#
+# Fijar directorio
+setwd()
+setwd("/Users/cesar.saavedra/Documents/GitHub/MLG-NP_Tarea2")
 #----------------------------------------------------------------------------------------#
 # Actividad 1
 #----------------------------------------------------------------------------------------#
@@ -47,14 +52,30 @@ suppressMessages(library(readr))
 # Actividad 2
 #----------------------------------------------------------------------------------------#
 # Cargar los datos
-Datos <- read.csv("/Users/cesar.saavedra/Documents/GitHub/MLG-NP_Tarea2/winequality-red.csv", sep = ";")
-Datos <- read.csv("C:/Users/Angie Rodr?guez/Desktop/Semestre 9/2. Modelos Lineales/MLG&NP_Tarea2/winequality-red.csv", sep = ";")
-
+Datos <- read.table("Datos.txt",header=T,sep = ",")
 Datos
+
+View(Datos)
+str(Datos)
 
 # Categrorizar variable pH a pHi
 summary(Datos[,"pH"])
 pHi <- cut(Datos$pH, breaks = c(2.6,3.1,3.6,4.1), labels = c("Bajo", "Medio", "Alto"))
 head(pHi)
 table(pHi)
+
+# Estadisticas descriptivas
+ggplot(data = Datos, aes(x=pHi, fill=pHi)) +
+  geom_bar(position="dodge") + 
+  ylab("") + xlab(" ") +
+  scale_fill_discrete(name = "Nivel pH:", labels = c("Bajo", "Medio", "Alto"))
+
+ggplot(Datos, aes(y=Datos$density, x=pHi, fill=pHi)) + geom_boxplot(show.legend = T) + 
+  scale_fill_discrete(name = "Ph:") +
+  xlab("") + ylab("Densidad")
+
+ggplot(Datos, aes(y=Datos$alcohol, x=Datos$quality, group=Datos$quality)) + geom_boxplot(show.legend = T)
+
+# Correlacion
+corrplot(cor(Datos), method = "number")
 
